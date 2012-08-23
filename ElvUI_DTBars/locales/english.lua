93b3f49c3df65998b86e5f4fa9edb4ec31da0c3b
Caedis <firebolt0777@yahoo.com>
2012-08-22 20:59:28 -0500

    Ignore for now

---
+++ ElvUI_DTBars/locales/english.lua
@@ -0,0 +1,20 @@
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+local L = LibStub("AceLocale-3.0"):GetLocale("ElvUI", false)
+
+if not L then return; end
+
+L["DTBars"] = "DTBars"
+L["DTBars Options"] = "DTBars Options"
+L["General"] = "General"
+L["Top Datatext Bar"] = "Top Datatext Bar"
+L["Enabled"] = "Enabled"
+L["Width"] = "Width"
+L["Bottom DataText Bar"] = "Bottom DataText Bar"
+L["Left Chat Datatext"] = "Left Chat Datatext"
+L["Right Chat Datatext"] = "Right Chat Datatext"
+L["Rep Datatext"] = "Rep Datatext"
+L["Mouseover"] = "Mouseover"
+L["Background Panels"] = "Background Panels"
+L["Top Panel"] = "Top Panel"
+L["Height"] = "Height"
+L["Bottom Panel"] = "Bottom Panel"
---
+++ ElvUI_DTBars/locales/german.lua
@@ -0,0 +1,3 @@
+local L = LibStub("AceLocale-3.0"):GetLocale("ElvUI_DTBars", false)
+if GetLocale() ~= 'deDE' then return; end
+if not L then return end
